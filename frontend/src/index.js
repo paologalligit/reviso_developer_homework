5c4b259ac523e47135dce50d5651bd2b5530471d
paologalligit <paolo>
2019-03-02 16:29:20 +0100

    Fixed index and routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,28 +5,27 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloLink, from } from 'apollo-link';
+import { ApolloLink } from 'apollo-link';
 
 import 'semantic-ui-css/semantic.min.css';
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
 
-const httpLink = createHttpLink({ uri: 'http://localhost:3000' });
+const httpLink = createHttpLink({ uri: 'http://localhost:8080/graphql' });
 
 const middlewareLink = setContext(() => ({
   headers: {
-    token: localStorage.getItem('token') || null,
-    refreshToken: localStorage.getItem('refreshToken') || null,
+    'x-token': localStorage.getItem('token'),
+    'x-refresh-token': localStorage.getItem('refreshToken'),
   },
 }));
 
-const afterwareLink = new ApolloLink((operation, forward) => forward(operation).map((response) => {
-  const context = operation.getContext();
-  const { response: { headers } } = context;
+const afterwareLink = new ApolloLink((operation, forward) => {
+  const { headers } = operation.getContext();
 
   if (headers) {
-    const token = headers.get('token');
-    const refreshToken = headers.get('refreshToken');
+    const token = headers.get('x-token');
+    const refreshToken = headers.get('x-refresh-token');
 
     if (token) {
       localStorage.setItem('token', token);
@@ -35,19 +34,14 @@ const afterwareLink = new ApolloLink((operation, forward) => forward(operation).
       localStorage.setItem('refreshToken', refreshToken);
     }
   }
-  return response;
-}));
 
+  return forward(operation);
+});
 
-// const middleLink = middlewareLink.concat(httpLink);
-// const afterLink = afterwareLink.concat(httpLink);
+const link = afterwareLink.concat(middlewareLink.concat(httpLink));
 
 const client = new ApolloClient({
-  link: from([
-    middlewareLink,
-    afterwareLink,
-    httpLink,
-  ]),
+  link,
   cache: new InMemoryCache(),
 });
 
@@ -58,8 +52,4 @@ const App = (
 );
 
 ReactDOM.render(App, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker();
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  BrowserRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
+import decode from 'jwt-decode';
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  try {
+    decode(token);
+    decode(refreshToken);
+  } catch (err) {
+    return false;
+  }
+
+  return true;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (isAuthenticated() ? (
+      <Component {...props} />
+    ) : (
+      <Redirect
+        to={{
+          pathname: '/login',
+        }}
+      />
+    ))}
+  />
+);
+
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/register" exact component={Register} />
+      <Route path="/login" exact component={Login} />
+      <PrivateRoute path="/view-team/:teamId?/:channelId?" exact component={ViewTeam} />
+      <PrivateRoute path="/create-team" exact component={CreateTeam} />
+    </Switch>
+  </BrowserRouter>
+);
