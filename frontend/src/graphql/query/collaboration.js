cdbddfb84ab86aef110b15c3eeb2bdc0728ee32e
paologalligit <paolo>
2019-03-23 15:08:35 +0100

    Added the end hour field into collaboration filter query

--- frontend/src/graphql/query/collaboration.js
+++ frontend/src/graphql/query/collaboration.js
@@ -3,11 +3,11 @@ import gql from 'graphql-tag';
 export default gql`
   query (
     $user_id: Int!, $customer_id: Int, $name: String, $budget: Float, $vat: Float, $penalty: Float, 
-      $date: String, $start_hour: String,
+      $date: String, $start_hour: String, $end_hour: String
   ) {
     filteredCollaborations (
       user_id: $user_id, customer_id: $customer_id, name: $name, budget: $budget, vat: $vat, 
-        penalty: $penalty, date: $date, start_hour: $start_hour
+        penalty: $penalty, date: $date, start_hour: $start_hour, end_hour: $end_hour
     ) {
       id,
       name,
