c092250e1ffd820ae6b25ac6804c718929c4b268
paologalligit <paolo.galli7@studenti.unimi.it>
2019-04-19 12:55:06 +0200

    Updated the db dump

--- freelancerdb.sql
+++ freelancerdb.sql
@@ -34,7 +34,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: collaborations; Type: TABLE; Schema: public; Owner: postgres
+-- Name: collaborations; Type: TABLE; Schema: public; Owner: paologio
 --
 
 CREATE TABLE public.collaborations (
@@ -57,10 +57,10 @@ CREATE TABLE public.collaborations (
 );
 
 
-ALTER TABLE public.collaborations OWNER TO postgres;
+ALTER TABLE public.collaborations OWNER TO paologio;
 
 --
--- Name: collaborations_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: collaborations_id_seq; Type: SEQUENCE; Schema: public; Owner: paologio
 --
 
 CREATE SEQUENCE public.collaborations_id_seq
@@ -71,17 +71,17 @@ CREATE SEQUENCE public.collaborations_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.collaborations_id_seq OWNER TO postgres;
+ALTER TABLE public.collaborations_id_seq OWNER TO paologio;
 
 --
--- Name: collaborations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: collaborations_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: paologio
 --
 
 ALTER SEQUENCE public.collaborations_id_seq OWNED BY public.collaborations.id;
 
 
 --
--- Name: customers; Type: TABLE; Schema: public; Owner: postgres
+-- Name: customers; Type: TABLE; Schema: public; Owner: paologio
 --
 
 CREATE TABLE public.customers (
@@ -100,10 +100,10 @@ CREATE TABLE public.customers (
 );
 
 
-ALTER TABLE public.customers OWNER TO postgres;
+ALTER TABLE public.customers OWNER TO paologio;
 
 --
--- Name: customers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: customers_id_seq; Type: SEQUENCE; Schema: public; Owner: paologio
 --
 
 CREATE SEQUENCE public.customers_id_seq
@@ -114,17 +114,17 @@ CREATE SEQUENCE public.customers_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.customers_id_seq OWNER TO postgres;
+ALTER TABLE public.customers_id_seq OWNER TO paologio;
 
 --
--- Name: customers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: customers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: paologio
 --
 
 ALTER SEQUENCE public.customers_id_seq OWNED BY public.customers.id;
 
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
+-- Name: users; Type: TABLE; Schema: public; Owner: paologio
 --
 
 CREATE TABLE public.users (
@@ -144,10 +144,10 @@ CREATE TABLE public.users (
 );
 
 
-ALTER TABLE public.users OWNER TO postgres;
+ALTER TABLE public.users OWNER TO paologio;
 
 --
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: paologio
 --
 
 CREATE SEQUENCE public.users_id_seq
@@ -158,120 +158,88 @@ CREATE SEQUENCE public.users_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.users_id_seq OWNER TO postgres;
+ALTER TABLE public.users_id_seq OWNER TO paologio;
 
 --
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: paologio
 --
 
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- Name: collaborations id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: collaborations id; Type: DEFAULT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.collaborations ALTER COLUMN id SET DEFAULT nextval('public.collaborations_id_seq'::regclass);
 
 
 --
--- Name: customers id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: customers id; Type: DEFAULT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.customers ALTER COLUMN id SET DEFAULT nextval('public.customers_id_seq'::regclass);
 
 
 --
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
 
 --
--- Data for Name: collaborations; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: collaborations; Type: TABLE DATA; Schema: public; Owner: paologio
 --
 
 COPY public.collaborations (id, name, budget, vat, penalty, date, start_hour, end_hour, payment, due_date, sent, delay, customer_id, created_at, updated_at, user_id) FROM stdin;
-14	Vediamo un po	9999	9	9	2019-04-01 15:51:07.489+02	15:51:07	15:51:07	\N	\N	f	\N	4	2019-04-01 15:51:16.644+02	2019-04-06 16:40:03.278+02	3
-12	Breaking bad	1000	45	12	2019-04-01 15:14:22.742+02	14:14:00	15:14:22	\N	\N	f	\N	4	2019-04-01 15:14:43.592+02	2019-04-06 16:40:19.72+02	3
-11	Atlantide	10000	100	10	2019-03-28 10:34:37.834+01	10:34:37	10:34:37	\N	\N	f	\N	4	2019-03-28 10:36:11.665+01	2019-04-06 16:41:12.044+02	3
-13	New project	4500	125	11	2019-04-01 15:19:21.208+02	15:19:21	15:19:21	\N	\N	f	\N	8	2019-04-01 15:20:11.879+02	2019-04-06 16:41:19.48+02	3
-15	Nuovo	1540	308	1225	2019-04-07 08:11:14.409+02	08:11:14	08:11:14	\N	\N	f	\N	11	2019-04-07 08:11:56.316+02	2019-04-07 08:11:56.316+02	3
-16	Nuovo bis	544	21.76	54	2019-04-07 08:12:35.919+02	08:12:35	08:12:35	\N	\N	f	\N	9	2019-04-07 08:12:56.214+02	2019-04-07 08:13:03.622+02	3
-19	Ampliamento cucina 	2000	400	14	2019-04-10 16:46:13.082+02	16:46:13	16:46:13	\N	\N	f	\N	3	2019-04-10 16:47:11.142+02	2019-04-10 16:47:11.142+02	2
-17	Pulizia scale	124	24.8	12	2019-04-07 08:14:23.876+02	08:14:23	08:14:23	\N	\N	f	\N	12	2019-04-07 08:15:10.192+02	2019-04-14 15:37:52.221+02	2
-18	Costruzione muro	1500	150	55	2019-04-10 16:43:05.086+02	16:43:05	16:43:05	\N	\N	t	\N	15	2019-04-10 16:43:47.529+02	2019-04-14 15:46:46.795+02	2
-20	Errore costruzione	2500	250	12	2019-04-10 17:01:30.296+02	16:01:00	17:01:30	\N	\N	t	\N	15	2019-04-10 17:02:09.068+02	2019-04-15 11:35:31.674+02	2
-21	Registrazione voce	150	30	55	2019-04-26 00:00:00+02	15:38:00	18:38:00	\N	\N	f	\N	19	2019-04-15 11:41:02.677+02	2019-04-15 11:41:02.677+02	2
+1	Fist project	1500	60	125	2019-04-26 00:00:00+02	16:00:00	18:30:00	\N	\N	f	\N	1	2019-04-19 12:27:00.003+02	2019-04-19 12:27:00.003+02	1
+2	Top Secret	200000	40000	500	2019-05-01 00:00:00+02	00:00:00	17:30:00	\N	\N	f	\N	2	2019-04-19 12:33:37.62+02	2019-04-19 12:33:37.62+02	1
 \.
 
 
 --
--- Name: collaborations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: collaborations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: paologio
 --
 
-SELECT pg_catalog.setval('public.collaborations_id_seq', 21, true);
+SELECT pg_catalog.setval('public.collaborations_id_seq', 2, true);
 
 
 --
--- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: paologio
 --
 
 COPY public.customers (id, name, surname, country, email, specialization, city, address, postal, created_at, updated_at, user_id) FROM stdin;
-1	Fuffolo	Baffo	i	baffo@email.com	i	i	i	9	2019-03-25 15:04:01.831+01	2019-03-25 15:04:01.831+01	1
-3	Impostore	Salvo	jkl	baffo@email.com	jkl	jkl	jkl	9	2019-03-25 15:15:29.492+01	2019-03-25 15:15:29.492+01	2
-4	GianRoberto	Cazzola	hjkdu	cazzola@email.com	nessuna	hidkj	via scemo	12	2019-03-28 10:27:55.969+01	2019-03-28 10:27:55.969+01	3
-5	fdsajkl	jkl	jkl	ldfjklas@email.com	jklò	fdas	jklò	89	2019-03-30 16:21:58.306+01	2019-03-30 16:21:58.306+01	1
-6	hjikljhjk	jhklhjlb	hjk	iougbi@e.com	hbko	gfdhj	hnjk	9	2019-03-30 16:23:08.292+01	2019-03-30 16:23:08.292+01	1
-7	llll	lllll	jklhjk	ooo@io.io	kgj	gjkgjk	gbkj	8	2019-03-30 16:27:17.72+01	2019-03-30 16:27:17.72+01	1
-8	Kira	Bello	Japan	bello@email.com	killer	Tokyo	via dell'albero bello	89	2019-04-01 15:20:04.428+02	2019-04-01 15:20:04.428+02	3
-9	Nessuno	Polifemo	jkl	polifemo@email.com	jkl	jkl	jhkl	8	2019-04-01 16:23:34.218+02	2019-04-01 16:23:34.218+02	3
-10	Pupillo	Brando	jklll	bra@email.com	nod	lllijkkhj	jhfkdslaklk	8900	2019-04-05 18:12:40.238+02	2019-04-05 18:12:40.238+02	1
-11	Pollo	Fino	c	finopollo@email.com		b	g	6	2019-04-07 08:11:50.838+02	2019-04-07 08:11:50.838+02	3
-12	Nuccio	Cardarelli	huk	nu@email.com		kdu	klj	8989	2019-04-07 08:14:57.679+02	2019-04-07 08:14:57.679+02	2
-13	Luca	Buono	jkl	luca@email.com		jkl	jkl	89	2019-04-07 09:03:01.494+02	2019-04-07 09:03:01.494+02	2
-14	Adolfo	Spruzzi	com	spu@email.com		it	kkkl	87	2019-04-07 09:04:21.737+02	2019-04-07 09:04:21.737+02	2
-15	Prova	Errore	jkl	error@email.com		jkl	jkl	98	2019-04-07 09:30:28.827+02	2019-04-07 09:30:28.827+02	2
-16	Customer	Customino	jkl	custo@email.com		jkl		8977	2019-04-07 09:31:23.616+02	2019-04-07 09:31:23.616+02	2
-17	Undef	Finded	ujkl	undef@email.com		jkl	hjvsdk	899	2019-04-07 09:34:23.563+02	2019-04-07 09:34:23.563+02	2
-19	Baffo	Banfi	jkl	baffo_banfi@email.com		jkl	jkl	9	2019-04-15 11:40:15.051+02	2019-04-15 11:40:15.051+02	2
-20	Baffo	Banfi	jkl	baffo@email.com	j	jkl	jkl	9	2019-04-15 11:42:43.427+02	2019-04-15 11:42:43.427+02	3
-23	Il	Bello	jkl	bello@email.com		jkl	jkl	54	2019-04-15 12:06:53.5+02	2019-04-15 12:06:53.5+02	10
-27	La	Ranocchia	jkl	rana@email.com	Cucina	jkl	via ciao, 88	90	2019-04-15 12:15:56.235+02	2019-04-15 12:15:56.235+02	10
+1	Robert	Customer	Customer	rob@email.com	Plumber	Cus	Customer Street, 123	7778	2019-04-19 12:26:05.444+02	2019-04-19 12:26:05.444+02	1
+2	George	Buff	USA	buff@email.com	Prog	New York	Majesty Square, 1987	123456	2019-04-19 12:33:01.89+02	2019-04-19 12:33:01.89+02	1
 \.
 
 
 --
--- Name: customers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: customers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: paologio
 --
 
-SELECT pg_catalog.setval('public.customers_id_seq', 27, true);
+SELECT pg_catalog.setval('public.customers_id_seq', 2, true);
 
 
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: paologio
 --
 
 COPY public.users (id, name, surname, username, email, birthdate, country, password, city, address, postal, created_at, updated_at) FROM stdin;
-1	Johnny	Bravo	cool	cool@email.com	2019-01-01	Italy	$2b$12$6nVCIwHp8uIfvnKJRzNA0.vXpfr02x7EyDqsBESYVJulBpkVzUEC.	Milan	Bello	32010	2019-03-25 15:03:14.598+01	2019-03-25 15:03:14.598+01
-2	Giorgio	Kelli	kelli	kelli@email.com	2019-01-01	jkl	$2b$12$8T2xnbEKt/WNxL4TuRh3BOf01JfuqS3rTDUARalK71la/FGGOZ6sC	jkl	jkl	9	2019-03-25 15:14:57.089+01	2019-03-25 15:14:57.089+01
-3	Lorenzo	Uppolo	uppolo	uppo@email.com	2019-01-01	Italy	$2b$12$M83hlIYnLGFXKlCe0pFJXuqGek1nAia4azVqY6EqWkv9vc1EZz9q6	Florence	via ciao	1200	2019-03-28 10:17:42.155+01	2019-03-28 10:17:42.155+01
-4	Claudio	Lippi	Clauditone456	cit456@email.com	2000-12-31	Italy	$2b$12$6M8M28iAJeNgjplNPMZ9auKeFAUxORIMETYa1Oa7zaflSUKgPfjnC	Milan	Via Dei Rognosi	20100	2019-03-31 10:08:23.784+02	2019-03-31 10:08:23.784+02
-8	Claudio	Lippi	Clauditone45116	c4it456@email.com	2000-12-31	Italy	$2b$12$jyOzTDsT6voqdXpjvWCLr.uzmHMz0yUJYsVkj0EMi5bWuR1O6pcT2	Milan	Via Dei Rognosi	20100	2019-03-31 10:25:45.984+02	2019-03-31 10:25:45.984+02
-10	Sandro	Puppo	puppo	puppo@email.com	2019-01-01	i	$2b$12$az9PRFaMFpjmLFCOn4CG.OOPOwPJAcEd9SQHV/p6rrYOyTLKsvsd2	i	i	4	2019-04-15 11:58:03.874+02	2019-04-15 11:58:03.874+02
+1	User	User	User	user@email.com	2000-01-01	Italy	$2b$12$s0u3GE5OxWlU.Askz1qWqOHedNobG4pJQ97oYqTpra29IXCEQ56Oy	Milan	User Road, 45	123456	2019-04-19 12:24:39.124+02	2019-04-19 12:24:39.124+02
 \.
 
 
 --
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: paologio
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 10, true);
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
 
 
 --
--- Name: collaborations collaborations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: collaborations collaborations_pkey; Type: CONSTRAINT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.collaborations
@@ -279,7 +247,7 @@ ALTER TABLE ONLY public.collaborations
 
 
 --
--- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.customers
@@ -287,7 +255,7 @@ ALTER TABLE ONLY public.customers
 
 
 --
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.users
@@ -295,7 +263,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.users
@@ -303,7 +271,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.users
@@ -311,14 +279,14 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: customers_email_user_id; Type: INDEX; Schema: public; Owner: postgres
+-- Name: customers_email_user_id; Type: INDEX; Schema: public; Owner: paologio
 --
 
 CREATE UNIQUE INDEX customers_email_user_id ON public.customers USING btree (email, user_id);
 
 
 --
--- Name: collaborations collaborations_customer_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: collaborations collaborations_customer_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.collaborations
@@ -326,7 +294,7 @@ ALTER TABLE ONLY public.collaborations
 
 
 --
--- Name: collaborations collaborations_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: collaborations collaborations_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.collaborations
@@ -334,7 +302,7 @@ ALTER TABLE ONLY public.collaborations
 
 
 --
--- Name: customers customers_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: customers customers_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: paologio
 --
 
 ALTER TABLE ONLY public.customers
